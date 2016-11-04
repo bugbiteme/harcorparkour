a0d6fe850fee3c0228c5f6d4348cf094c29a40d9
Leon Levy <leon.s.levy@gmail.com>
2016-11-03 21:17:46 -0700

    second commit

---
+++ desktop/src/com/hard/core/parkour/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.hard.core.parkour.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.hard.core.parkour.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
---
+++ html/src/com/hard/core/parkour/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.hard.core.parkour.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.hard.core.parkour.MyGdxGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new MyGdxGame();
+        }
+}
